df1e9cad23438a16d1b7c3c8636753c41bef8e1e
Ayemere Odia <Odiadavid2@gmail.com>
2020-02-20 15:34:10 +0100

    Initial Commit

---
+++ TakeAway/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TakeAway
+//
+//  Created by Ayemere  Odia  on 20/02/2020.
+//  Copyright © 2020 Ayemere  Odia . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
